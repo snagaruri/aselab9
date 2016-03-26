bd1ae2a8c72be6341a82f7cdef49325880bdc766
sudheerase <nagarurisudheer91@gmail.com>
2016-03-25 21:30:23 -0400

    ok

---
+++ Code/www/js/thread.js
@@ -0,0 +1,29 @@
+angular.module('rtfmApp')
+.controller('threadPageController', function($scope, $location, $routeParams, threadService, userService){
+    var threadId = $routeParams.threadId;
+
+    $scope.newComment = '';
+    var thread = threadService.getThread(threadId);
+
+    thread.$bindTo($scope, 'thread') //creates $scope.thread with 3 way binding
+
+    $scope.addComment= function(){ 
+        if(!$scope.newComment){
+            return false; //Don't do anything if the text box is empty
+        }       
+
+        var currentUser = userService.getLoggedInUser();
+
+        var newComment = {
+            text: $scope.newComment,
+            username: currentUser.name
+        };
+
+        $scope.thread.comments = $scope.thread.comments || [];
+        $scope.thread.comments.push(newComment);
+    
+        $scope.newComment = ''; //Clear the input box
+    }
+
+
+});
